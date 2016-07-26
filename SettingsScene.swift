802c40e07f6216fd6ccd057f23c436bfe79bfa16
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2016-07-26 21:20:56 +0700

    Added a setting for music.

--- SettingsScene.swift
+++ SettingsScene.swift
@@ -9,9 +9,10 @@
 import SpriteKit
 
 enum SettingsOption: Int {
-    case Back = 0
+    case Music = 0
+    case Back
     
-    static let allValues: [SettingsOption] = [Back]
+    static let allValues: [SettingsOption] = [Music, Back]
 }
 
 protocol SettingsSceneDelegate: SKSceneDelegate {
@@ -20,6 +21,7 @@ protocol SettingsSceneDelegate: SKSceneDelegate {
 
 class SettingsScene: BaseScene {
     private var backLabel: SKLabelNode!
+    private var musicLabel: SKLabelNode!
 
     private var selectedOption: SettingsOption = .Back
 
@@ -59,9 +61,13 @@ class SettingsScene: BaseScene {
         let x = self.size.width / 2
         let y = self.size.height / 2
         
-        self.backLabel = SKLabelNode(text: "Back")
-        self.backLabel.position = CGPoint(x: x, y: y + 150)
+        self.backLabel = SKLabelNode(text: "BACK")
+        self.backLabel.position = CGPoint(x: x, y: y - 50)
         self.addChild(self.backLabel)
+        
+        self.musicLabel = SKLabelNode(text: "MUSIC")
+        self.musicLabel.position = CGPoint(x: x, y: y + 50)
+        self.addChild(self.musicLabel)
     }
     
     private func updateUI() {
@@ -70,7 +76,7 @@ class SettingsScene: BaseScene {
         
         for option in SettingsOption.allValues {
             let label = labelForSettingsOption(option)
-            label.fontName = option == self.selectedOption ? highlightFontName : defaultFontName
+            label.fontName = (option == self.selectedOption) ? highlightFontName : defaultFontName
         }
     }
     
@@ -79,12 +85,47 @@ class SettingsScene: BaseScene {
         
         switch option {
         case .Back: label = self.backLabel
+        case .Music: label = self.musicLabel
         }
         
         return label
     }
     
+    // MARK: - Public
+    
+    override func handleUpPress(forPlayer player: PlayerIndex) {
+        var selectionIndex = 0
+        
+        for (idx, option) in SettingsOption.allValues.enumerate() {
+            if option == self.selectedOption {
+                selectionIndex = max(idx - 1, 0)
+                break
+            }
+        }
+        
+        self.selectedOption = SettingsOption(rawValue: selectionIndex)!
+        
+        updateUI()
+    }
+    
+    override func handleDownPress(forPlayer player: PlayerIndex) {
+        var selectionIndex = 0
+        
+        for (idx, option) in SettingsOption.allValues.enumerate() {
+            if option == self.selectedOption {
+                selectionIndex = min(idx + 1, SettingsOption.allValues.count - 1)
+                break
+            }
+        }
+        
+        self.selectedOption = SettingsOption(rawValue: selectionIndex)!
+        
+        updateUI()
+    }
+
     override func handleActionPress(forPlayer player: PlayerIndex) {
-        self.settingsSceneDelegate?.settingsScene(self, optionSelected: self.selectedOption)
+        if self.selectedOption == .Back {
+            self.settingsSceneDelegate?.settingsScene(self, optionSelected: self.selectedOption)
+        }
     }
 }
